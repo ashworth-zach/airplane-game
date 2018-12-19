eaa7aafaea2b0a141757972b6714f9851341597f
ashworth-zach <39654996+ashworth-zach@users.noreply.github.com>
2018-12-19 10:52:20 -0600

    added momentum... still debugging

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ var enemies = [
 	{left:100, top:100},
 	{left:450, top:250}
 ]
+var velY = 0,
+    velX = 0,
+    speed = 2, // max speed
+    friction = 0.98, // friction
+    keys = [];
 var missiles =[];
 var shotsfired=0;
 function drawPlayer(){
@@ -16,28 +21,57 @@ function drawPlayer(){
 	document.getElementById('players').innerHTML = content;
 }
 document.onkeydown = function(e){
-	console.log(e.keyCode);
+	// console.log(e.keyCode);
 	
-	if(e.keyCode == 39){
-		player.left=player.left+5
-	}
-	if(e.keyCode == 37){
-		player.left=player.left-5
-	}
-	if(e.keyCode == 38){
-		player.top=player.top-5
-		if(player.top<450){
-			player.top=player.top+5;
-		}
-	}
-	if(e.keyCode == 40){
-		player.top=player.top+5
-	}
+	// if(e.keyCode == 39){
+	// 	player.left=player.left+5
+
+	// }
+	// if(e.keyCode == 37){
+	// 	player.left=player.left-5
+
+	// }
+	// if(e.keyCode == 38){
+
+	// 	player.top=player.top-5
+	// 	if(player.top<450){
+	// 		player.top=player.top+5;
+	// 	}
+	// }
+	// if(e.keyCode == 40){
+	// 	player.top=player.top+5
+
+	// }
+	if (keys[38]) {
+        if (velY > -speed) {
+            velY--;
+        }
+    }
+    
+    if (keys[40]) {
+        if (velY < speed) {
+            velY++;
+        }
+    }
+    if (keys[39]) {
+        if (velX < speed) {
+            velX++;
+        }
+    }
+    if (keys[37]) {
+        if (velX > -speed) {
+            velX--;
+        }
+    }
 	if(e.keyCode == 32){
 		shotsfired=shotsfired+1;
 		missiles.push({left: (player.left +35) ,top: (player.top)});
 		document.getElementById('shots').innerHTML = "<strong>Total Shots: " + shotsfired + "</strong>";
 	}
+    velY *= friction;
+    player.top += velY;
+    velX *= friction;
+    player.left += velX;
 
 }
 function drawEnemies(){
@@ -67,20 +101,22 @@ function moveMissiles(){
 	for(var i = 0; i<missiles.length;i++)
 	{
 		missiles[i].top=missiles[i].top-1;
+		if(missiles[i].top<0){
+			missiles.splice(i,1);
+		}
 	}
 }
-var kills = [];
+var kills = 0;
 function checkImpact(){
 
 	for(var i = 0; i<missiles.length;i++)
 	{
 		for(var x = 0; x<enemies.length;x++){
 			if(missiles[i].left >= enemies[x].left-35 && missiles[i].left <= enemies[x].left+35 && missiles[i].top <= enemies[x].top) //strange collision bug plz help
-			{//strange collision bug, only happens on second or third plane shot.
+			{ 
 				enemies.splice(x,1);
-				kills.push(1);
-				document.getElementById('kills').innerHTML= "kills: "+kills.length;
-				return;
+				kills+=1;
+				document.getElementById('kills').innerHTML= "kills: "+kills;
 			}
 		}		
 	}
@@ -92,7 +128,12 @@ function gameLoop(){
 	drawMissile();
 	moveMissiles();
 	checkImpact();
-	
 	setTimeout(gameLoop, 10);
 }
 gameLoop();
+document.body.addEventListener("keydown", function (e) {
+    keys[e.keyCode] = true;
+});
+document.body.addEventListener("keyup", function (e) {
+    keys[e.keyCode] = false;
+});
