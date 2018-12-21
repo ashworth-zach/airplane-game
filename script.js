bc81850fe1b2325e03db107c054fb38b57fbdfd9
ashworth-zach <39654996+ashworth-zach@users.noreply.github.com>
2018-12-21 11:04:47 -0600

    uploading

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 var player = {
-	left:450,
-	top:620
+	left: 450,
+	top: 620
 }
-var enemies = [
-	{left:650, top:100},
-	{left:350, top:400},
-	{left:100, top:220},
-	{left:100, top:100},
-	{left:450, top:250}
+var enemies = [{
+		left: 650,
+		top: 100
+	},
+	{
+		left: 350,
+		top: 400
+	},
+	{
+		left: 100,
+		top: 220
+	},
+	{
+		left: 100,
+		top: 100
+	},
+	{
+		left: 450,
+		top: 250
+	}
 ]
 var velY = 0,
-    velX = 0,
-    speed = 2, // max speed
-    friction = 0.98, // friction
-    keys = [];
-var missiles =[];
-var shotsfired=0;
-function drawPlayer(){
-	var content = "<div class='player' style='left:" +player.left + "px; top:" + player.top + "px;'></div>";
+	velX = 0,
+	speed = 2, // max speed
+	friction = 0.98, // friction
+	keys = [];
+var missiles = [];
+var shotsfired = 0;
+
+function drawPlayer() {
+	var content = "<div class='player' style='left:" + player.left + "px; top:" + player.top + "px;'></div>";
 	document.getElementById('players').innerHTML = content;
 }
-document.onkeydown = function(e){
+document.onkeydown = function (e) {
 	// console.log(e.keyCode);
-	
+
 	// if(e.keyCode == 39){
 	// 	player.left=player.left+5
 
@@ -43,85 +58,90 @@ document.onkeydown = function(e){
 
 	// }
 	if (keys[38]) {
-        if (velY > -speed) {
-            velY--;
-        }
-    }
-    
-    if (keys[40]) {
-        if (velY < speed) {
-            velY++;
-        }
-    }
-    if (keys[39]) {
-        if (velX < speed) {
-            velX++;
-        }
-    }
-    if (keys[37]) {
-        if (velX > -speed) {
-            velX--;
-        }
-    }
-	if(e.keyCode == 32){
-		shotsfired=shotsfired+1;
-		missiles.push({left: (player.left +35) ,top: (player.top)});
-		document.getElementById('shots').innerHTML = "<strong>Total Shots: " + shotsfired + "</strong>";
+		if (velY > -speed) {
+			velY--;
+		}
 	}
-    velY *= friction;
-    player.top += velY;
-    velX *= friction;
-    player.left += velX;
+
+	if (keys[40]) {
+		if (velY < speed) {
+			velY++;
+		}
+	}
+	if (keys[39]) {
+		if (velX < speed) {
+			velX++;
+		}
+	}
+	if (keys[37]) {
+		if (velX > -speed) {
+			velX--;
+		}
+	}
+	if (e.keyCode == 32) {
+		shotsfired += 1;
+		missiles.push({
+			left: (player.left + 35),
+			top: (player.top)
+		});
+		document.getElementById('shots').innerHTML = "<strong>Total shots: " + shotsfired + "</strong>";
+	}
+	velY *= friction;
+	player.top += velY;
+	velX *= friction;
+	player.left += velX;
 
 }
-function drawEnemies(){
-	var content1='';
-	for(var i = 0; i<enemies.length;i++)
-	{
-	content1+= "<div class='enemy' style='left:" +enemies[i].left + "px; top:" + enemies[i].top + "px;'></div>";
-	//var content2 = "<div class='enemy1' style='left:" +enemies[1].left + "px; top:" + enemies[1].top + "px;'></div>";
-	document.getElementById('enemies').innerHTML = content1;
+
+function drawEnemies() {
+	var content1 = '';
+	for (var i = 0; i < enemies.length; i++) {
+		content1 += "<div class='enemy' style='left:" + enemies[i].left + "px; top:" + enemies[i].top + "px;'></div>";
+		//var content2 = "<div class='enemy1' style='left:" +enemies[1].left + "px; top:" + enemies[1].top + "px;'></div>";
+		document.getElementById('enemies').innerHTML = content1;
 	}
 }
-function drawMissile(){
-	var content='';
-	for(var i = 0; i<missiles.length;i++){
-	content+= "<div class='missile' style='left:" +missiles[i].left + "px; top:" + missiles[i].top + "px;'></div>";
-	document.getElementById('missiles').innerHTML = content;
+
+function drawMissile() {
+	var content = '';
+	for (var i = 0; i < missiles.length; i++) {
+		content += "<div class='missile' style='left:" + missiles[i].left + "px; top:" + missiles[i].top + "px;'></div>";
+		document.getElementById('missiles').innerHTML = content;
 	}
 }
-function moveEnemies(){   
-	for(var i = 0; i<enemies.length;i++)
-	{
-	enemies[i].top= enemies[i].top+.1;
+
+function moveEnemies() {
+	for (var i = 0; i < enemies.length; i++) {
+		enemies[i].top = enemies[i].top + .1;
 	}
 }
-function moveMissiles(){
-	
-	for(var i = 0; i<missiles.length;i++)
-	{
-		missiles[i].top=missiles[i].top-1;
-		if(missiles[i].top<0){
-			missiles.splice(i,1);
+
+function moveMissiles() {
+
+	for (var i = 0; i < missiles.length; i++) {
+		missiles[i].top = missiles[i].top - 1;
+		if (missiles[i].top < 0) {
+			missiles.splice(i, 1);
 		}
 	}
 }
 var kills = 0;
-function checkImpact(){
 
-	for(var i = 0; i<missiles.length;i++)
-	{
-		for(var x = 0; x<enemies.length;x++){
-			if(missiles[i].left >= enemies[x].left-35 && missiles[i].left <= enemies[x].left+35 && missiles[i].top <= enemies[x].top) //strange collision bug plz help
-			{ 
-				enemies.splice(x,1);
-				kills+=1;
-				document.getElementById('kills').innerHTML= "kills: "+kills;
+function checkImpact() {
+
+	for (var i = 0; i < missiles.length; i++) {
+		for (var x = 0; x < enemies.length; x++) {
+			if (missiles[i].left >= enemies[x].left - 35 && missiles[i].left <= enemies[x].left + 35 && missiles[i].top <= enemies[x].top) //strange collision bug plz help
+			{
+				enemies.splice(x, 1);
+				kills += 1;
+				document.getElementById('kills').innerHTML = "kills: " + kills;
 			}
-		}		
+		}
 	}
 }
-function gameLoop(){
+
+function gameLoop() {
 	drawPlayer();
 	moveEnemies();
 	drawEnemies();
@@ -132,8 +152,8 @@ function gameLoop(){
 }
 gameLoop();
 document.body.addEventListener("keydown", function (e) {
-    keys[e.keyCode] = true;
+	keys[e.keyCode] = true;
 });
 document.body.addEventListener("keyup", function (e) {
-    keys[e.keyCode] = false;
+	keys[e.keyCode] = false;
 });
